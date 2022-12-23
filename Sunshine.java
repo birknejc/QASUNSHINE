9a051de88f99aee0d416da66e637537e579b405f
harrisonBirkner <57458202+harrisonBirkner@users.noreply.github.com>
2022-12-22 21:07:16 -0600

    updated code with date logic

--- Sunshine.java
+++ Sunshine.java
@@ -1,12 +1,17 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Scanner;
 import java.text.DecimalFormat;
-import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class Sunshine {
     static Scanner inputDevice;
     public static void main(String[] args) {
         init();
+        String strDate = getDate();
+        formatDate(strDate);
         double iMinutes = input();
         double cHours = Calcs1(iMinutes);
         double cMinutes = Calcs2(iMinutes);
@@ -16,7 +21,9 @@ public class Sunshine {
 
     public static void output(double cHours, double cMinutes, double cCost) {
         LocalDate today = LocalDate.now();
-        System.out.println("Current Date: " + today);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
+        String formattedDate = today.format(dateTimeFormatter);
+        System.out.println("Current Date: " + formattedDate);
         System.out.println(CompanyMotto.getMotto());
         System.out.format("%12s%12s    %12s\n","Hours","Minutes","Rental Cost");
         DecimalFormat df = new DecimalFormat("$#,###.##");
@@ -39,6 +46,31 @@ public class Sunshine {
         double cCost = ((cHours-cHours%1)*40)+iMinutes%60;
         return cCost;
     }
+    public static String getDate(){
+        String enteredDate;
+        System.out.print("Enter the rental date MM/DD/YYYY: ");
+        enteredDate = inputDevice.nextLine();
+        return enteredDate;
+    }
+
+    public static String formatDate(String enteredDate){
+        String enteredPattern = "MM/DD/YYYY";
+        String newPattern = "MMM dd, yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(enteredPattern);
+        try{
+            Date d = sdf.parse(enteredDate);
+            sdf.applyPattern(newPattern);
+            String newDateString = sdf.format(d);
+            System.out.println("Rental date: " + newDateString);
+        }
+        catch (ParseException pe){
+            System.out.println("Invalid date entered");
+        }
+        return enteredDate;
+
+
+    }
+
 
     public static double input() {
         double iMinutes = 0;
